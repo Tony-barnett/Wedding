efb4b0ab731840bbdab91ff1cf6eef4319beff9b
Tony Barnett <tony.barnett@ukplc.net>
2016-11-09 21:49:02 +0000

    Upload initial state
    This is unchanged from the standard MVC web app with identity framework

---
+++ WeddingPlanning/WeddingPlanning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeddingPlanning.Startup))]
+namespace WeddingPlanning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
