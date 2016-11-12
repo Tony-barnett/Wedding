a3c8ebd6b131b81573e1dec9af5eda80c18c6013
Tony Barnett <tony.barnett@ukplc.net>
2016-11-12 19:30:21 +0000

    Add RSVP pages
    There's two pages, a yes and a no. The no is split into two partials because we want to put two different models on it, one for adding guests and one for adding small children

---
+++ WeddingPlanning/WeddingPlanning/Controllers/RSVPController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WeddingPlanning.Models;
+
+namespace WeddingPlanning.Controllers
+{
+    public class RSVPController : Controller
+    {
+        // GET: RSVP
+        public ActionResult RSVP()
+        {
+            var dropDownOptions = Enumerable.Range(0, 10).Select(thing => new SelectListItem { Text = thing.ToString(), Value = thing.ToString() }).ToList();
+            ViewData["ChildrenViewModel"] = new ChildrenViewModel { DropDownItems = dropDownOptions };
+            ViewData["GuestViewModel"] = new GuestViewModel { IsComing = true };
+
+            ViewBag.ReturnUrl = "#";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddChildren()
+        {
+
+        }
+
+        //GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CannotCome()
+        {
+            ViewBag.ReturnUrl = "#";
+            return View(new GuestViewModel { IsComing = false });
+        }
+    }
+}
---
+++ WeddingPlanning/WeddingPlanning/Models/RSVPModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace WeddingPlanning.Models
+{
+    public class GuestViewModel
+    {
+
+        [Required]
+        public bool IsComing { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Allergies")]
+        public string Allergies { get; set; }
+    }
+
+    public class ChildrenViewModel
+    {
+        public IEnumerable<SelectListItem> DropDownItems { get; set; }
+        [Display(Name = "Young Children")]
+        public int Babies { get; set; }
+
+        [Display(Name = "Children")]
+        public int Children { get; set; }
+    }
+}
