bb53a3518fa0a228d480eaf7e4faebd37c97023c
Tony Barnett <tony.barnett@ukplc.net>
2016-12-04 21:05:08 +0000

    WIP: Add a guest manager and guest store
    I do quite like the manager-store pattern... Everything talks to the manager which in turn talks to the store, the store then decides which storage provider is going to be user etc. I don't believe these are quite finished.

---
+++ WeddingPlanning/WeddingPlanning/GuestStore/GuestManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WeddingPlanning.Models;
+
+namespace WeddingPlanning.GuestStore
+{
+    public class GuestManager
+    {
+        private IGuestStore _GuestStore {get;set;}
+
+        public GuestManager(IGuestStore guestStore = null)
+        {
+            _GuestStore = guestStore ?? new GuestStore();
+        }
+
+        public async Task AddGuest(GuestViewModel guest, int? storerId = null)
+        {
+            _GuestStore.AddGuest(guest, storerId);
+        }
+
+        public async Task<IEnumerable<GuestViewModel>> GetGuests()
+        {
+            return _GuestStore.GetGuests();
+        }
+
+        public async Task AddChild(ChildrenViewModel child, int storerId)
+        {
+            _GuestStore.AddChild(child, storerId);
+        }
+
+        public async Task<IEnumerable<ChildrenViewModel>> GetChildren()
+        {
+            return _GuestStore.GetChildren();
+        }
+    }
+}
---
+++ WeddingPlanning/WeddingPlanning/GuestStore/GuestStore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeddingPlanning.Models;
+using WeddingPlanning.StuffStorage;
+
+namespace WeddingPlanning.GuestStore
+{
+    public interface IGuestStore
+    {
+        IEnumerable<GuestViewModel> GetGuests();
+
+        IEnumerable<ChildrenViewModel> GetChildren();
+
+        void AddGuest(GuestViewModel guest, int? StorerId = null);
+
+        void AddChild(ChildrenViewModel child, int StorerId);
+
+    }
+
+    public class GuestStore: IGuestStore
+    {
+        private IStorageProvider _StorageProvider;
+
+        public GuestStore(IStorageProvider storageProvider = null)
+        {
+            _StorageProvider = storageProvider ?? new CSVStorer();
+        }
+        public IEnumerable<GuestViewModel> GetGuests()
+        {
+            return new List<GuestViewModel>();
+        }
+
+        public IEnumerable<ChildrenViewModel> GetChildren()
+        {
+            return new List<ChildrenViewModel>();
+        }
+
+        public void AddGuest(GuestViewModel guest, int? storerId = null)
+        {
+            _StorageProvider.StoreGuest(guest, storerId);
+        }
+
+        public void AddChild(ChildrenViewModel child, int storerId)
+        {
+            _StorageProvider.StoreChild(child, storerId);
+        }
+
+    }
+}
