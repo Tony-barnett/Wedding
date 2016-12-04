67e1154f1bf7fea6971f2d03f983deb803bdd292
Tony Barnett <tony.barnett@ukplc.net>
2016-12-04 21:02:54 +0000

    Add some tests for the CsvStorer
    The easiest way to do it, write a file to disk and read it in. It's not ideal, if there's an issue with the writer then the reader tests will also fail...

--- WeddingPlanning/WeddingPlanning.IntegrationTests/Properties/AssemblyInfo.cs
+++ WeddingPlanning/WeddingPlanning.IntegrationTests/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WeddingPlanning")]
+[assembly: AssemblyTitle("WeddingPlanning.IntegrationTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WeddingPlanning")]
+[assembly: AssemblyProduct("WeddingPlanning.IntegrationTests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1a09e563-4053-4063-af5d-7a00f87a664e")]
+[assembly: Guid("8b321c6a-408f-4781-8415-104917ffaecb")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("WeddingPlanning.IntegrationTests")]
---
+++ WeddingPlanning/WeddingPlanning.IntegrationTests/StuffStorage/CsvStorerTests.cs
@@ -0,0 +1,198 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeddingPlanning;
+
+namespace WeddingPlanning.IntegrationTests.StuffStorage
+{
+    [TestFixture]
+    class CsvStorerTests
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            var csvStorer = new WeddingPlanning.StuffStorage.CSVStorer();
+
+            File.Delete(csvStorer._Location);
+        }
+
+        [Test]
+        public void TestWritePersonToCsv_WithId_PersonAddedToCsv()
+        {
+            var id = 1;
+            var guestViewModel = new WeddingPlanning.Models.GuestViewModel
+            {
+                Id = id,
+                FirstName = "foo",
+                Surname = "bar",
+                IsComing = true,
+                Allergies = "Some"
+            };
+
+            var csvStorer = new WeddingPlanning.StuffStorage.CSVStorer();
+
+            csvStorer.StoreGuest(guestViewModel);
+
+            using (StreamReader reader = new StreamReader(csvStorer._Location))
+            {
+                var file = reader.ReadToEnd();
+
+                Assert.AreEqual($"\"{id}\"", file.Substring(0, 3));
+            }
+        }
+
+        [Test]
+        public void TestWritePersonToCsv_TwoPeopleWithId_PeopleAddedToCsv()
+        {
+            var guestViewModel = new WeddingPlanning.Models.GuestViewModel
+            {
+                Id = 1,
+                FirstName = "foo",
+                Surname = "dillyDoo",
+                IsComing = true,
+                Allergies = "Some"
+            };
+
+            var guestViewModel2 = new WeddingPlanning.Models.GuestViewModel
+            {
+                Id = 2,
+                FirstName = "bar",
+                Surname = "baram",
+                IsComing = true,
+                Allergies = "none, guv",
+                AddedBy = 1
+            };
+
+            var csvStorer = new WeddingPlanning.StuffStorage.CSVStorer();
+
+            var id = csvStorer.StoreGuest(guestViewModel);
+            csvStorer.StoreGuest(guestViewModel2);
+
+            Assert.AreEqual(1, id);
+
+            using (StreamReader reader = new StreamReader(csvStorer._Location))
+            {
+                var file = reader.ReadToEnd();
+
+                var peopleInFile = file.Replace("\r", "").Split('\n');
+                Assert.AreEqual($"\"{1}\"", peopleInFile[0].Substring(0, 3));
+                Assert.AreEqual($"\"{2}\"", peopleInFile[1].Substring(0, 3));
+            }
+        }
+
+        [Test]
+        public void TestWritePersonToCsv_WithoutId_PersonAddedToCsv()
+        {
+            var guestViewModel = new WeddingPlanning.Models.GuestViewModel
+            {
+                FirstName = "foo",
+                Surname = "dillyDoo",
+                IsComing = true,
+                Allergies = "Some"
+            };
+
+            var csvStorer = new WeddingPlanning.StuffStorage.CSVStorer();
+
+            var id = csvStorer.StoreGuest(guestViewModel);
+
+            Assert.AreEqual(1, id);
+
+            using (StreamReader reader = new StreamReader(csvStorer._Location))
+            {
+                var file = reader.ReadToEnd();
+
+                var peopleInFile = file.Replace("\r", "").Split('\n');
+                Assert.AreEqual($"\"{1}\"", peopleInFile[0].Substring(0, 3));
+            }
+        }
+
+        [Test]
+        public void TestReadPersonFromCsv_OnePersonAdded_GuestReturned()
+        {
+            var firstName = "foo";
+            var surname = "bar";
+            var guestViewModel = new WeddingPlanning.Models.GuestViewModel
+            {
+                FirstName = firstName,
+                Surname = surname,
+                IsComing = true,
+                Allergies = "Some"
+            };
+
+            var csvStorer = new WeddingPlanning.StuffStorage.CSVStorer();
+
+            var id = csvStorer.StoreGuest(guestViewModel);
+
+            var foundUser = csvStorer.GetGuest(firstName, surname);
+
+            Assert.AreEqual(id, foundUser.Id);
+
+        }
+
+        [Test]
+        public void TestReadPersonFromCsv_GetSecondPersonAdded_GuestReturned()
+        {
+
+            var guestViewModel = new WeddingPlanning.Models.GuestViewModel
+            {
+                FirstName = "bar",
+                Surname = "baram",
+                IsComing = true,
+                Allergies = "none, guv"
+            };
+
+            var firstName = "foo";
+            var surname = "bar";
+            var id = 2;
+            var guestViewModel2 = new WeddingPlanning.Models.GuestViewModel
+            {
+                Id = id,
+                FirstName = firstName,
+                Surname = surname,
+                IsComing = true,
+                AddedBy = 1,
+                Allergies = "Some"
+            };
+
+            var csvStorer = new WeddingPlanning.StuffStorage.CSVStorer();
+
+            csvStorer.StoreGuest(guestViewModel);
+            csvStorer.StoreGuest(guestViewModel2);
+
+            var foundUser = csvStorer.GetGuest(firstName, surname);
+
+            Assert.AreEqual(id, foundUser.Id);
+        }
+
+        [Test]
+        public void TestReadPersonFromCsv_CsvDoesntExist_ReturnsNull()
+        {
+            var csvStorer = new WeddingPlanning.StuffStorage.CSVStorer();
+            var foundUser = csvStorer.GetGuest("no users exist", "so these don't matter");
+            Assert.IsNull(foundUser);
+        }
+
+        [Test]
+        public void TestReadPersonFromCsv_PersonNotInCsv_ReturnsNUll()
+        {
+            var csvStorer = new WeddingPlanning.StuffStorage.CSVStorer();
+
+            var firstName = "foo";
+            var surname = "bar";
+            var guestViewModel = new WeddingPlanning.Models.GuestViewModel
+            {
+                FirstName = firstName,
+                Surname = surname,
+                IsComing = true,
+                Allergies = "Some"
+            };
+
+            var foundUser = csvStorer.GetGuest("This is not the user", "You're looking for");
+            Assert.IsNull(foundUser);
+        }
+    }
+}
