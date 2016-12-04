db5d5cf29795a4f8b93f091c2b9a6b9236acd1ba
Tony Barnett <tony.barnett@ukplc.net>
2016-12-04 21:27:51 +0000

    Add some logic around adding people
    It's pretty much just calls to the GuestManager

--- WeddingPlanning/WeddingPlanning/Controllers/RSVPController.cs
+++ WeddingPlanning/WeddingPlanning/Controllers/RSVPController.cs
@@ -4,29 +4,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WeddingPlanning.GuestStore;
 using WeddingPlanning.Models;
 
 namespace WeddingPlanning.Controllers
 {
     public class RSVPController : Controller
     {
+        private GuestManager _GuestManager;
+        public RSVPController() {
+            _GuestManager = new GuestManager();
+        }
+
+
         // GET: RSVP
         public ActionResult RSVP()
         {
-            var dropDownOptions = Enumerable.Range(0, 10).Select(thing => new SelectListItem { Text = thing.ToString(), Value = thing.ToString() }).ToList();
-            ViewData["ChildrenViewModel"] = new ChildrenViewModel { DropDownItems = dropDownOptions };
-            ViewData["GuestViewModel"] = new GuestViewModel { IsComing = true };
+            var storerId = ViewData["StorerId"];
 
-            ViewBag.ReturnUrl = "#";
+            ViewData["ChildrenViewModel"] = new ChildrenViewModel { AddedBy= (int?)storerId };
+            ViewData["GuestViewModel"] = new GuestViewModel { IsComing = true, AddedBy= (int?)storerId };
+
+            ViewBag.ReturnUrl = "/RSVP/AddGuest";
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> AddChildren()
+        public async Task<ActionResult> AddGuest(GuestViewModel guest, int? storerId = null)
         {
-            // TODO: add a GuestManager and GuestStore, then plumb them in here.
-            return View();
+            storerId = await _GuestManager.AddGuest(guest, storerId);
+            ViewData["StorerId"] = storerId;
+            return Redirect("/RSVP/RSVP");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddChildren(ChildrenViewModel child, int storerId)
+        {
+            await _GuestManager.AddChild(child, storerId);
+            return Redirect("/RSVP/RSVP");
         }
 
         //GET: Index
