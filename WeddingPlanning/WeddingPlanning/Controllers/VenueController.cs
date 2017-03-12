764fa22f7c38e979e2125510051369f95f2904bd
Tony Barnett <tony.barnett@ukplc.net>
2017-03-12 15:07:59 +0000

    Add controller for venue info

--- WeddingPlanning/WeddingPlanning/Controllers/VenueController.cs
+++ WeddingPlanning/WeddingPlanning/Controllers/VenueController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,5 +9,17 @@ namespace WeddingPlanning.Controllers
 {
     public class VenueController: Controller
     {
+        public async Task<ActionResult> Index()
+        {
+            var guestId = GuestStore.Helpers.GetStorerIdFromCookie(Request);
+
+            if(guestId == null)
+            {
+                throw new InvalidOperationException("Attempted to access stuff without being a valid guest");
+            }
+
+            var guest = await new GuestStore.GuestManager().GetGuest(guestId.Value);
+            return View(guest);
+        }
     }
 }
