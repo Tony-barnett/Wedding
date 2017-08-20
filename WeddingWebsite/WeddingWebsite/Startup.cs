94118d708b90b0bf44d39b60cf19dfa5d0edba3d
Tony Barnett <anthony.barnett21@gmail.com>
2017-08-20 16:39:14 +0100

    Add routing rule to force all RSVP urls back to index action in .net
    We let angular handle anything after the RSVP, this just ensures that if someone goes to /RSVP/foo then they still see the RSVP section. It's a bit clunky as it means that if you go to can't come and refresh it'll take you back to the root...

--- WeddingWebsite/WeddingWebsite/Startup.cs
+++ WeddingWebsite/WeddingWebsite/Startup.cs
@@ -104,6 +104,10 @@ namespace WeddingWebsite
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "RSVP", 
+                    template: "RSVP/{*anything}", 
+                    defaults: new { controller = "RSVP", action = "Index" });
             });
         }
 
