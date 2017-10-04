25420e8a13b7114d20faa0dca1c17da8d23c450b
Tony Barnett <anthony.barnett21@gmail.com>
2017-10-04 21:09:58 +0100

    Require log in for all but the home page

--- WeddingWebsite/WeddingWebsite/Controllers/ItineraryController.cs
+++ WeddingWebsite/WeddingWebsite/Controllers/ItineraryController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace WeddingWebsite.Controllers
 {
+    [Authorize]
     public class ItineraryController : Controller
     {
         //GET: Index
--- WeddingWebsite/WeddingWebsite/Controllers/VenueController.cs
+++ WeddingWebsite/WeddingWebsite/Controllers/VenueController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using WeddingWebsite.GuestStore;
 
 namespace WeddingWebsite.Controllers
 {
+    [Authorize]
     public class VenueController: Controller
     {
         private IGuestManager _GuestManager;
